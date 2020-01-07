8c69177b292efc48d04dd197e6db7f271106577e
hjl19910901 <18710360575@163.com>
2020-01-07 20:12:20 +0800

    java算法实现

---
+++ src/leetcode/TestArray.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class TestArray {
+
+    public static void main(String[] args) {
+
+        int[] arr1=new int[3];
+
+        int length1=arr1.length;
+        System.out.println("arr1's length:"+length1);
+
+        //访问元素
+        int element0 = arr1[0];
+        System.out.println("element0:"+element0);
+
+    }
+}
