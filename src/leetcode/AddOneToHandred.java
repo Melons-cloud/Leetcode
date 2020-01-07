65133b679608c23e321c32627a2d84ec5ac0b834
hjl19910901 <18710360575@163.com>
2020-01-07 20:06:45 +0800

    java算法实现

---
+++ src/leetcode/AddOneToHandred.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class AddOneToHandred {
+    public static void main(String[] args) {
+
+        int total=0;
+        int end=100;
+
+        for (int i=0;i<=end;i++){
+
+            total+=i;
+        }
+
+        System.out.println(total);
+
+
+    }
+}
